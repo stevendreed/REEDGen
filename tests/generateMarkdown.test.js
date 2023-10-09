82916a8d710bbe9c9086abb80befb3c1e438b908
stevendreed <steven.daniel.reed@gmail.com>
2023-10-09 13:43:21 -0700

    moved test file to correct dir

--- tests/generateMarkdown.test.js
+++ tests/generateMarkdown.test.js
@@ -1,3 +1,32 @@
-const generateMarkdown = require(`./generateMarkdown.js`);
+const generateMarkdown = require(`../utils/generateMarkdown`);
 
 
+describe(`renderLicenseBadge`, () =>
+{
+    it(`should return an empty string if there is no license`, () =>
+    {
+        const testData = ``; // TODO: identify what licenses are invalid
+        const returnExp = ``; // expecting an empty string
+        let returnAct = renderLicenseBadge(testData);
+
+        expect(returnAct).toEqual(returnExp);
+    } // end funct
+    ); // end it
+
+} // end funct
+); // end describe
+describe(`renderLicenseLink`, () =>
+{
+
+} // end funct
+); // end describe
+describe(`renderLicenseSection`, () =>
+{
+
+} // end funct
+); // end describe
+describe(`generateMarkdown`, () =>
+{
+
+} // end funct
+); // end describe
