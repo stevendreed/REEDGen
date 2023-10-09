1739f15808d889057d9ff358770879045de4148a
stevendreed <steven.daniel.reed@gmail.com>
2023-10-09 13:37:02 -0700

    added describe functs for each genmd funct

--- utils/generateMarkdown.test.js
+++ utils/generateMarkdown.test.js
@@ -1,3 +1,23 @@
 const generateMarkdown = require(`../utils/generateMarkdown.js`);
 
 
+describe(`renderLicenseBadge`, () =>
+{
+
+} // end funct
+); // end describe
+describe(`renderLicenseLink`, () =>
+{
+
+} // end funct
+); // end describe
+describe(`renderLicenseSection`, () =>
+{
+
+} // end funct
+); // end describe
+describe(`generateMarkdown`, () =>
+{
+
+} // end funct
+); // end describe
