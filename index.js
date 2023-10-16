d1cd2e58d1a5fe66937d0cd753cb7f04568db144
stevendreed <steven.daniel.reed@gmail.com>
2023-10-15 19:10:51 -0700

    added questions obj

--- index.js
+++ index.js
@@ -1,7 +1,68 @@
 // TODO: Include packages needed for this application
 const inquirer = require(`inquirer`);
 // TODO: Create an array of questions for user input
-const questions = [];
+const licenseObj =
+[
+    {
+        name: `None`,
+        keyw: `N/A`,
+        url: ``,
+    },
+    {
+        name: `Academic Free License v3.0`,
+        keyw: `AFL-3.0`,
+        url: ``,
+    },
+    {
+        name: `Apache license 2.0`,
+        keyw: `Apache-2.0`,
+        url: ``,
+    },
+]
+const questions =
+[
+    {
+        name: `title`,
+        type: `input`,
+        message: `Enter title of your repository:`,
+    },
+    {
+        name: `description`,
+        type: `input`,
+        message: `Enter a description for your repository:`,
+    },
+    {
+        name: `installation`,
+        type: `input`,
+        message: `Enter installation instructions for your repository`,
+    },
+    {
+        name: `usage`,
+        type: `input`,
+        message: `Enter instructions on how to use your repository`,
+    },
+    {
+        name: `contribution`,
+        type: `input`,
+        message: `Enter instructions on how to contribute to your repository`,
+    },
+    {
+        name: `license`,
+        type: `list`,
+        message: `Choose a license you'd like to use for your repository`,
+        choices: licenseObj,
+    },
+    {
+        name: `author`,
+        type: `input`,
+        message: ``,
+    },
+    {
+        name: `email`,
+        type: `input`,
+        message: ``,
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
